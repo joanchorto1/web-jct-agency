6e3c526932cabd158bc888c1eb153d7c8d11bf9b
alumne <joanchorto1@iesebre.com>
2025-08-25 12:31:15 +0200

    Canviar logo footeer

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/header';
-import logoImage from '../img/LOGO(Fondo Transparent).png';
+import logoImage from '../img/LOGO JCTAGENCY.png';
 
 const Layout = ({ children }) => (
   <>
