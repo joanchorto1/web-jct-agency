eb4612bd520b784c83a4919cbf8985abebf3e4d8
joanchorto1 <joanchorto1@iesebre.com>
2026-06-03 09:08:50 +0200

    Fix hydration crash in prerendered app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
@@ -15,11 +15,11 @@ const app = (
   </React.StrictMode>
 );
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, app);
-} else {
-  createRoot(rootElement).render(app);
-}
+// The site is prerendered for SEO, but runtime libraries such as AOS mutate
+// the HTML snapshot before React boots. Rendering from a clean root avoids
+// hydration mismatches in production while keeping the prerendered HTML for bots.
+rootElement.innerHTML = '';
+createRoot(rootElement).render(app);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
